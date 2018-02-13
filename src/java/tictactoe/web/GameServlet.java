7935250b30ad2fadfb892e9a4bf8cd8e11f6956d
Wagner Macedo <wagnerluis1982@gmail.com>
2018-02-12 21:16:03 -0300

    Cria o GameServlet em /play-game
    Eu explico sobre os mapeamentos da URL.
    
        // Atendendendo por endereços diferentes
        @WebServlet(urlPatterns = {"/play-game", "/GameShow"})
    
    Aqui também é o momento para relembrar como o browser busca os recursos
    e para falar sobre o caminho do contexto da aplicação e o motivo de o
    porque `<link rel="stylesheet" href="/game.css"/>` não funciona.
    
        // Mapeamento por subcaminhos: Problema com o CSS!!!
        @WebServlet(urlPatterns = {"/play-game", "/jogos/velha"})
    
        // Para o CSS funcionar, utilizamos a tag c:url
        <link rel="stylesheet" href="<c:url value="/game.css"/>">

---
+++ src/java/tictactoe/web/GameServlet.java
@@ -0,0 +1,29 @@
+package tictactoe.web;
+
+import java.io.IOException;
+import java.util.HashMap;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet(urlPatterns = {"/play-game"})
+public class GameServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession();
+
+        Object squares = session.getAttribute("gameSquares");
+        if (squares == null) {
+            session.setAttribute("gameSquares", new HashMap());
+        }
+
+        RequestDispatcher jsp = request.getRequestDispatcher("/index.jsp");
+        jsp.forward(request, response);
+    }
+
+}
