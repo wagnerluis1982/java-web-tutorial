462f807bbbbfd40b2f164f3fd2953d38607ae989
Wagner Macedo <wagnerluis1982@gmail.com>
2018-02-15 19:48:49 -0300

    Armazena o histórico do tabuleiro
    É uma boa oportunidade de mostrar o debug funcionando, começando pela
    primeira linha do método `clickSquare()` abaixo:
    
        public void clickSquare(int index) {
            Character[] squares = this.getSquares();

--- src/java/tictactoe/web/GameApp.java
+++ src/java/tictactoe/web/GameApp.java
@@ -1,10 +1,17 @@
 package tictactoe.web;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GameApp {
 
-    private Character[] squares = new Character[9];
+    private List<Character[]> history = new ArrayList<>(1);
     private char turn = 'X';
     private char winner = ' ';
+
+    public GameApp() {
+        this.history.add(new Character[9]);
+    }
 
     public void clickSquare(String param) {
         if (param != null) {
@@ -14,17 +21,23 @@ public class GameApp {
     }
 
     public void clickSquare(int index) {
+        Character[] squares = this.getSquares();
+
         // Parando se já tiver um vencedor ou o quadrado já estiver clicado
-        if (this.winner != ' ' || this.squares[index] != null) {
+        if (this.winner != ' ' || squares[index] != null) {
             return;
         }
 
         // Modifica uma cópia do array
-        Character[] squares = this.squares.clone();
+        squares = squares.clone();
         squares[index] = this.turn;
 
+        // Modifica uma cópia do histórico
+        List<Character[]> history = new ArrayList<>(this.history);
+        history.add(squares);
+
         // Atualiza o estado do jogo
-        this.squares = squares;
+        this.history = history;
         this.turn = (this.turn == 'X') ? 'O' : 'X';
         this.winner = calculateWinner(squares);
     }
@@ -57,7 +70,7 @@ public class GameApp {
     }
 
     public Character[] getSquares() {
-        return this.squares;
+        return this.history.get(this.history.size() - 1);
     }
 
     public char getTurn() {
